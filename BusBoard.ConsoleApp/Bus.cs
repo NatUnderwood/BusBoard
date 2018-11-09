d95ef57d7105b7138fcd2397366ab9f29d62ad1c
Natalie Underwood <Natalie.Underwood@softwire.com>
2018-11-09 11:36:57 +0000

    First Commit

---
+++ BusBoard.ConsoleApp/Bus.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BusBoard.ConsoleApp
+{
+    public class Bus
+    {
+        public string LineName;
+        public string DestinationName;
+        public DateTime ExpectedArrival;
+
+        public Bus(string lineName, string destinationName, DateTime expectedArrival)
+        {
+            DestinationName = destinationName;
+            ExpectedArrival = expectedArrival;
+            LineName = lineName;
+        }
+
+        public override string ToString()
+        {
+            return $"Line Name: {LineName} Destination Name: {DestinationName} Expected Arrival: {ExpectedArrival.ToShortTimeString()}";
+        }
+    }
+}
--- BusBoard.ConsoleApp/Program.cs
+++ BusBoard.ConsoleApp/Program.cs
@@ -1,15 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using RestSharp;
+using Newtonsoft.Json;
 
 namespace BusBoard.ConsoleApp
+
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
+        static void Main(string[] args)
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+            var client = new RestClient("https://api.tfl.gov.uk/");
+            var request = new RestRequest("/StopPoint/{id}/Arrivals", Method.GET);
+            request.AddUrlSegment("id", "490008660N");
+            IRestResponse response = client.Execute(request);
+            var content = response.Content;
+            var listOfBuses=JsonConvert.DeserializeObject<List<BusJson>>(content);
+            listOfBuses.RemoveRange(5, listOfBuses.Count - 5);
+            foreach (var bus in listOfBuses)
+            {
+                Console.WriteLine(bus.ToBus().ToString());
+            }
+        }
     }
-  }
 }
